588cd900d3656ab2982211d445c1a2d2d6608cfb
Paul <Paul.Chapuis@etu.uca.fr>
2020-06-02 20:09:55 +0200

    Correction d'un bug empechant la vérification de l'autorité de certification.

--- server/Server/sslserver.cpp
+++ server/Server/sslserver.cpp
@@ -13,12 +13,13 @@ void sslServer::incomingConnection(qintptr socketDescriptor)
         if (serverSocket->setSocketDescriptor(socketDescriptor)) {
             addPendingConnection(serverSocket);
             QObject::connect(serverSocket, qOverload<const QList<QSslError> &>(&QSslSocket::sslErrors), this, &sslServer::sslErrors, Qt::DirectConnection);
-            serverSocket->setLocalCertificate("/etc/letsencrypt/live/paulcorporation.space/fullchain.pem");
+            serverSocket->setLocalCertificateChain(QSslCertificate::fromPath("/etc/letsencrypt/live/paulcorporation.space/fullchain.pem", QSsl::Pem));
+            qDebug() << "certificats trouvés : "<<  QString::number(serverSocket->localCertificateChain().size());
             serverSocket->setPrivateKey("/etc/letsencrypt/live/paulcorporation.space/privkey.pem", QSsl::Rsa, QSsl::Pem);
             qDebug() << serverSocket->localCertificate().version();
             qDebug() << serverSocket->localCertificate().publicKey();
             qDebug() << serverSocket->localCertificate().expiryDate();
-            serverSocket->setProtocol(QSsl::TlsV1_2);
+            serverSocket->setProtocol(QSsl::TlsV1_2OrLater);
             serverSocket->startServerEncryption();
             QObject::connect(serverSocket, &QSslSocket::disconnected, serverSocket, &QObject::deleteLater);
             emit newPendingConnexion(serverSocket);
