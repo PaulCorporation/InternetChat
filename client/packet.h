1c2b854f03955db4bd78bc8c2b5a7fe456e8eb00
Paul <Paul.Chapuis@etu.uca.fr>
2020-06-04 15:31:06 +0200

    Add packet subclasses system

---
+++ client/packet.h
@@ -0,0 +1,56 @@
+#ifndef PACKET_H
+#define PACKET_H
+#include <QDataStream>
+class packet
+{
+public:
+    packet();
+    virtual void operator<<(QDataStream&) = 0;
+    virtual void operator>>(QDataStream&) = 0;
+protected:
+    quint8 id;
+};
+class loginRequest : public packet
+{
+public:
+    loginRequest();
+    virtual void operator<<(QDataStream&) override;
+    virtual void operator>>(QDataStream&) override;
+    void setMail(QString mail);
+    void setPassword(QString password);
+    QString getMail();
+    QString getPassword();
+private :
+    QString m_mail;
+    QString m_password;
+};
+class signupRequest : public packet
+{
+public:
+    signupRequest();
+    virtual void operator<<(QDataStream&) override;
+    virtual void operator>>(QDataStream&) override;
+    void setMail(QString mail);
+    void setPassword(QString password);
+    void setUser(QString user);
+    QString getMail();
+    QString getPassword();
+    QString getUser();
+    QString m_user;
+    QString m_password;
+    QString m_mail;
+};
+class message : public packet
+{
+public:
+    message();
+    virtual void operator<<(QDataStream&) override;
+    virtual void operator>>(QDataStream&) override;
+    void setUser(QString user);
+    void setContent(QString content);
+    QString getUser();
+    QString getContent();
+    QString m_user;
+    QString m_content;
+};
+#endif // PACKET_H
